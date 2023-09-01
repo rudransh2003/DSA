ae5bd65a7e54131e12d8b954384c0ded10f274ec
Rudransh Singh Chouhan <singhchouhanrudransh@gmail.com>
2023-09-02 00:47:12 +0530

    first commit

---
+++ PatternProblems/pyramid.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    //full pyramid
+// int n =5;
+
+// for(int row=0; row<5;row++){
+//         for(int col=0;col<5-row-1;col++){
+//         cout<<" ";
+//     }
+//     for(int col=0;col<row+1;col++){
+//         cout<<"* ";
+//     }
+
+//     cout<<endl;
+// }
+
+    //inverted F.P.
+// int n =5;
+
+// for(int row=0; row<5;row++){
+//         for(int col=0;col<row+1;col++){
+//         cout<<" ";
+//     }
+//     for(int col=0;col<5-row-1;col++){
+//         cout<<"* ";
+//     }
+
+//     cout<<endl;
+// }
+
+//Hollow P.
+
+int n ;
+
+for(int row=0; row<5;row++){
+        for(int col=0;col<n-row-1;col++){
+        cout<<" ";
+}
+   
+   
+   
+    
+
+}
+
+    return 0;
+}
